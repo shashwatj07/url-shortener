2ea5105b3c097ac24f3e6ecbf3da84082fbbe0c7
Dhruv88 <dhruva4000@gmail.com>
2022-01-15 17:46:40 +0530

    custom-url working

--- main.go
+++ main.go
@@ -4,25 +4,21 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"log"
 	"math/big"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 const hostUrl = "http://localhost:8080/"
 const hostPort = "localhost:8080"
 
-var algo = sha256.New()
-
 func sha256Of(input string) []byte {
+	var algo = sha256.New()
 	algo.Write([]byte(input))
+	// log.Printf("%x\n", algo.Sum(nil))
 	return algo.Sum(nil)
-}
-
-type urlStruct struct {
-	LongURL  string `json:"longUrl"`
-	ShortURL string `json:"shortUrl"`
-
 }
 
 var repo = NewDynamoDBRepository()
@@ -30,11 +26,26 @@ var dynamoDBClient = createDynamoDBClient()
 
 func Encode(msg string) string {
 	urlHashBytes := sha256Of(msg)
-	println(urlHashBytes)
+	// println(urlHashBytes)
 	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
-	println(generatedNumber)
+	// println(generatedNumber)
 	encoded := base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%d", generatedNumber)))
 	return encoded[:6]
+}
+
+func saveUrlToDbandRespond(c *gin.Context, newUrlStruct Urlpair, shortUrl string) {
+	var temp Urlpair = newUrlStruct
+	temp.ShortURL = shortUrl
+	newUrlStruct.ShortURL = hostUrl+shortUrl
+	// TO DO: X = Check if longUrl already exists and return that if it does
+	_,error := repo.Save(&temp)
+	if error!=nil {
+		// panic(error)		// TO DO remove panic and try to send error as string in json below
+		log.Println(error)
+		c.AbortWithStatus(500)
+	} else {
+		c.IndentedJSON(http.StatusCreated, newUrlStruct)
+	}
 }
 
 func PostUrl(c *gin.Context) {
@@ -48,20 +59,29 @@ func PostUrl(c *gin.Context) {
 	if newUrlStruct.ShortURL != "" {
 		//TO DO
 		//custom shortURL
+		var shortUrl string = newUrlStruct.ShortURL
+		
+		pair,error := repo.FindByID(shortUrl)
+		if error!=nil {
+			log.Println(error)
+			c.AbortWithStatus(500)
+		} else {
+			LongUrl := pair.LongURL
+			switch LongUrl{
+				case "" :
+					saveUrlToDbandRespond(c,newUrlStruct, shortUrl)
+				case newUrlStruct.LongURL:
+					newUrlStruct.ShortURL = hostUrl+shortUrl
+					c.IndentedJSON(http.StatusCreated, newUrlStruct)
+				default:
+					c.AbortWithStatusJSON(http.StatusConflict, gin.H{ "error" : "This Custom URL is not available"})
+			}
+		}
+
 	} else {
 		var shortUrl = Encode(newUrlStruct.LongURL)
-		var temp Urlpair = newUrlStruct
-		temp.ShortURL = shortUrl
-		newUrlStruct.ShortURL = hostUrl+shortUrl
-		// TO DO: X = Check if longUrl already exists and return that if it does
-		_,error := repo.Save(&temp)
-		if error!=nil {
-			panic(error)		// TO DO remove panic and try to send error as string in json below
-			c.AbortWithStatusJSON(500, gin.H{"error": error})
-		} else {
-			c.IndentedJSON(http.StatusCreated, newUrlStruct)
-		}
-		newUrlStruct.ShortURL = hostUrl+shortUrl
+		saveUrlToDbandRespond(c, newUrlStruct, shortUrl)
+		// newUrlStruct.ShortURL = hostUrl+shortUrl
 	}
 }
 
@@ -69,10 +89,15 @@ func HandleShortUrlRedirect(c *gin.Context) {
 	shortUrl := c.Param("shortUrl")
 	pair,error := repo.FindByID(shortUrl)
 	if error!=nil {
-		c.AbortWithStatusJSON(500, gin.H{"error": error})	// TO DO error is currently returning empty, fix it
+		log.Println(error)
+		c.AbortWithStatus(500)	// TO DO error is currently returning empty, fix it
 	} else {
 		initialUrl := pair.LongURL
-		c.Redirect(302, initialUrl)
+		if(initialUrl != ""){
+			c.Redirect(302, initialUrl)
+		} else {
+			c.AbortWithStatus(404)
+		}
 	}
 }
 
