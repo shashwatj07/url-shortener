34eb80eac89aba63f83e903d39276c3fd594f3f6
Dhruv88 <dhruva4000@gmail.com>
2022-01-16 13:17:33 +0530

    expiry date added

--- entity.go
+++ entity.go
@@ -1,6 +1,11 @@
 package main
 
+// import "encoding/json"
+
+// "time"
+
 type Urlpair struct {
 	ShortURL    string  `json:"short_url"`
 	LongURL  string `json:"long_url"`
+	ExpDate int `json:"exp_date"`
 }
--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/base64"
+	// "encoding/json"
 	"fmt"
 	"log"
 	"math/big"
 	"net/http"
+	"time"
 
+	// "time"
 	"github.com/gin-gonic/gin"
 )
 
@@ -17,7 +20,6 @@ const hostPort = "localhost:8080"
 func sha256Of(input string) []byte {
 	var algo = sha256.New()
 	algo.Write([]byte(input))
-	// log.Printf("%x\n", algo.Sum(nil))
 	return algo.Sum(nil)
 }
 
@@ -26,9 +28,7 @@ var dynamoDBClient = createDynamoDBClient()
 
 func Encode(msg string) string {
 	urlHashBytes := sha256Of(msg)
-	// println(urlHashBytes)
 	generatedNumber := new(big.Int).SetBytes(urlHashBytes).Uint64()
-	// println(generatedNumber)
 	encoded := base64.URLEncoding.EncodeToString([]byte(fmt.Sprintf("%d", generatedNumber)))
 	return encoded[:6]
 }
@@ -36,15 +36,26 @@ func Encode(msg string) string {
 func saveUrlToDbandRespond(c *gin.Context, newUrlStruct Urlpair, shortUrl string) {
 	var temp Urlpair = newUrlStruct
 	temp.ShortURL = shortUrl
-	newUrlStruct.ShortURL = hostUrl+shortUrl
-	// TO DO: X = Check if longUrl already exists and return that if it does
-	_,error := repo.Save(&temp)
-	if error!=nil {
-		// panic(error)		// TO DO remove panic and try to send error as string in json below
-		log.Println(error)
+	newUrlStruct.ShortURL = hostUrl + shortUrl
+	days := newUrlStruct.ExpDate
+	if days<1 {
 		c.AbortWithStatus(500)
 	} else {
-		c.IndentedJSON(http.StatusCreated, newUrlStruct)
+		tempTime := time.Now().AddDate(0,0,days)
+		temp.ExpDate = int(tempTime.Unix())
+
+		/*
+		TO DO: X = Check if longUrl already exists and return that if it does(not needed according to our logic
+		only one random url possible and many custom urls possible for one long url
+		*/
+		_, error := repo.Save(&temp)
+		if error != nil {
+			// DONE remove panic and try to send error as string in json below
+			log.Println(error)
+			c.AbortWithStatus(500)
+		} else {
+			c.IndentedJSON(http.StatusCreated, newUrlStruct)
+		}
 	}
 }
 
@@ -57,43 +68,43 @@ func PostUrl(c *gin.Context) {
 	}
 	// Add the new album to the slice.
 	if newUrlStruct.ShortURL != "" {
-		//TO DO
+		//DONE
 		//custom shortURL
 		var shortUrl string = newUrlStruct.ShortURL
-		
-		pair,error := repo.FindByID(shortUrl)
-		if error!=nil {
+
+		//Check if the given custom URL is available
+		pair, error := repo.FindByID(shortUrl)
+		if error != nil {
 			log.Println(error)
 			c.AbortWithStatus(500)
 		} else {
 			LongUrl := pair.LongURL
-			switch LongUrl{
-				case "" :
-					saveUrlToDbandRespond(c,newUrlStruct, shortUrl)
-				case newUrlStruct.LongURL:
-					newUrlStruct.ShortURL = hostUrl+shortUrl
-					c.IndentedJSON(http.StatusCreated, newUrlStruct)
-				default:
-					c.AbortWithStatusJSON(http.StatusConflict, gin.H{ "error" : "This Custom URL is not available"})
+			switch LongUrl {
+			case "":
+				saveUrlToDbandRespond(c, newUrlStruct, shortUrl)
+			case newUrlStruct.LongURL:
+				newUrlStruct.ShortURL = hostUrl + shortUrl
+				c.IndentedJSON(http.StatusCreated, newUrlStruct)
+			default:
+				c.AbortWithStatusJSON(http.StatusConflict, gin.H{"error": "This Custom URL is not available"})
 			}
 		}
 
 	} else {
 		var shortUrl = Encode(newUrlStruct.LongURL)
 		saveUrlToDbandRespond(c, newUrlStruct, shortUrl)
-		// newUrlStruct.ShortURL = hostUrl+shortUrl
 	}
 }
 
 func HandleShortUrlRedirect(c *gin.Context) {
 	shortUrl := c.Param("shortUrl")
-	pair,error := repo.FindByID(shortUrl)
-	if error!=nil {
+	pair, error := repo.FindByID(shortUrl)
+	if error != nil {
 		log.Println(error)
-		c.AbortWithStatus(500)	// TO DO error is currently returning empty, fix it
+		c.AbortWithStatus(500) // DONE error is currently returning empty, fix it
 	} else {
 		initialUrl := pair.LongURL
-		if(initialUrl != ""){
+		if initialUrl != "" {
 			c.Redirect(302, initialUrl)
 		} else {
 			c.AbortWithStatus(404)
