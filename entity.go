2dbfeeafb1010d8999822dd4f9eb6cbefb6a00b1
Dhruv88 <dhruva4000@gmail.com>
2022-01-17 01:27:01 +0530

    extra line removed

--- entity.go
+++ entity.go
@@ -4,7 +4,6 @@ package main
 // LongURL: The url to be shortened
 // ExpDate: In the request it is the number of days after which the url expires
 // 		    In the response it is the UNIX epoch timestamp of the expiry date
-
 type urlStruct struct {
 	LongURL  string `json:"long_url"`
 	ShortURL string `json:"short_url"`
