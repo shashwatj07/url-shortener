a8f8927d039eb4810955080a9b00aab9539fa8c6
Dhruv88 <dhruva4000@gmail.com>
2022-01-24 17:30:58 +0530

    Added authorization to use Get Analytics API

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	router := gin.Default()
 	router.POST("/", AuthorizationMiddleware(), PostUrl)
 	router.GET("/:shortUrl", Redirect)
-	router.GET("/analytics/:shortUrl", GetAnalytics)
+	router.GET("/analytics/:shortUrl", AuthorizationMiddleware(), GetAnalytics)
 	router.GET("/auth/token", AuthorizationMiddleware(), CreateToken)
 	router.Run(HOST_PORT)
 }
