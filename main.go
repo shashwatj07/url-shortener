52b4510dc0cee79a36e1187e77c56c2dc78dedba
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2022-01-25 13:59:10 +0530

    bulk shortening api added

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 	router.DELETE("/:shortUrl", AuthorizationMiddleware(), DeleteUrl)
 	router.GET("/:shortUrl", Redirect)
 	router.GET("/auth/token", AuthorizationMiddleware(), CreateToken)
+	router.POST("/bulk", PostBulkUrl)
 	router.Run(HOST_PORT)
 }
