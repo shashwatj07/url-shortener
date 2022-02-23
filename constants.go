f432d7967e9e1dafb26159d4cce8e45e9ffaefd4
harshv46 <harshvardhan21364@gmail.com>
2022-02-23 10:53:07 +0530

    updated

--- constants.go
+++ constants.go
@@ -1,10 +1,10 @@
 package main
 
 // The host URL
-const HOST_URL = "http://localhost:8080/"
+const HOST_URL = "http://url-shortener2-dev.ap-south-1.elasticbeanstalk.com/"
 
 // The host port
-const HOST_PORT = "localhost:8080"
+const HOST_PORT = ":5000"
 
 // Default validity days
 const DEFAULT_VALIDITY_DAYS = 30
