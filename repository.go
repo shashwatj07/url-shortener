b3aad276a77580be7ab09ae2e7e9e50972ed13d8
harshv46 <harshvardhan21364@gmail.com>
2022-03-12 12:28:23 +0530

    changed config

--- repository.go
+++ repository.go
@@ -35,8 +35,8 @@ func NewDynamoDBRepository() PostRepository {
 func createDynamoDBClient() *dynamodb.DynamoDB {
 	// Create AWS Session
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
 		Config: aws.Config{Region: aws.String("ap-south-1")},
+		SharedConfigState: session.SharedConfigEnable,
 	}))
 
 	// Return DynamoDB client
