7825329965e19f9f561a998e6d9d092fb424c0ca
harshv46 <harshvardhan21364@gmail.com>
2022-03-12 11:31:32 +0530

    upadated deploy

--- constants.go
+++ constants.go
@@ -1,7 +1,7 @@
 package main
 
 // The host URL
-const HOST_URL = "http://url-shortener2-dev.ap-south-1.elasticbeanstalk.com/"
+const HOST_URL = "http://url-shortener4-dev.ap-south-1.elasticbeanstalk.com/"
 
 // The host port
 const HOST_PORT = ":5000"
